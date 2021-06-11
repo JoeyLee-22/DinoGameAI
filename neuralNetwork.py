4a7d3f51480939314e4dbe806529a081d9bcea10
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2021-06-11 11:42:08 -0700

    add print statements

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,8 +4,9 @@ import numpy as np
 
 class NeuralNetwork():
 
-    def __init__(self, dimensions=[0,0,0], learning_rate=0): 
+    def __init__(self, dimensions=[0,0,0], learningRate=0): 
         self.dimensions = dimensions
+        self.learningRate = learningRate
 
         self.secondLayerNeurons = np.empty(dimensions[1])
         self.outputNeurons = np.empty(dimensions[2])
@@ -50,28 +51,38 @@ class NeuralNetwork():
 
     def change(self):
         self.b2 -= self.learningRate * self.db2
-        self.w2 -= self.learningRate * (self.dw2 + self.Lambda * self.w2)
+        self.w2 -= self.learningRate * self.dw2
         self.b1 -= self.learningRate * self.db1
-        self.w1 -= self.learningRate * (self.dw1 + self.Lambda * self.w1)
+        self.w1 -= self.learningRate * self.dw1
 
         self.dw1 = np.zeros([self.dimensions[1], self.dimensions[0]])
         self.dw2 = np.zeros([self.dimensions[2], self.dimensions[1]])
         self.db1 = np.zeros(self.dimensions[1])
         self.db2 = np.zeros(self.dimensions[2])
 
-    def train(self, trainImages, trainLabels):
-        for m in range (self.bs):
-            correct_output = np.zeros([self.dimensions[2]])
-            correct_output[trainLabels[m]] = 1.0
+    def train(self, inputs, outputs, epochs=5):
+        accuracy = 0
+        err_sum = 0.0
+        avg_err = 0.0
+        correct = 0
+        for _ in range (epochs):
+            for m in range(len(outputs)):
+                correct_output = outputs[m]
+                self.forwardProp(inputs[m])
+                self.backProp(inputs[m], correct_output)
+                
+                if np.argmax(self.outputNeurons) == np.argmax(correct_output):
+                    correct+=1
 
-            self.forwardProp(trainImages[m].flatten())
-            self.backProp(trainImages[m].flatten(), correct_output)
+                error = np.amax(np.absolute(self.outputLayerErrors))
+                err_sum += error
+                
+            self.change()
+        
+        avg_err = err_sum / (epochs*len(outputs)+1)
+        accuracy = str(int((correct/(epochs*len(outputs)+1))*100)) + '%'  
+        print ("Accuracy: " + accuracy + " - Loss: " + str(round(avg_err, 10)))
 
-        self.change()
-
-    def predict(self, testImage):
-        self.forwardProp(testImage)
+    def predict(self, inputs):
+        self.forwardProp(inputs)
         return np.argmax(self.outputNeurons)
-
-if __name__ == "__main__":
-    nn = NeuralNetwork(dimensions = [5, 16, 3], learning_rate = 1e-5)
